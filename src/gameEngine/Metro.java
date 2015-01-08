e4dd0582a69744165e4727aaae201ab343b59020
Kenneth Frandsen <Frandsen86@gmail.com>
2015-01-08 13:49:23 +0100

    småkommentar i Metro

--- src/gameEngine/Metro.java
+++ src/gameEngine/Metro.java
@@ -1,11 +1,10 @@
 package gameEngine;
 
-
 public class Metro extends Felt{
 	
 	
 		
-		private int metroFine = 750;
+		private int metroFine = 750; //Set fine to 750
 		
 		public Metro(String fieldName){
 			super(fieldName);
@@ -17,8 +16,8 @@ public class Metro extends Felt{
 		@Override
 		public void landOnField(Spiller playerWhoLanded) {
 			int result = (int)(Math.random()*100);
-			if(result >= 20 && result <=40){
-				playerWhoLanded.getAccount().withdraw(metroFine);
+			if(result >= 20 && result <=40){ // Make i a 1 out of 5 chance to get a fine
+				playerWhoLanded.getAccount().withdraw(metroFine); //withdraw to the players account
 				System.out.println("Du har fået en bøde, for at køre uden billet.");
 			}
 			
