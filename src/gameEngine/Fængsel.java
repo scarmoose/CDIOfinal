c02be57ef8d06246c22d1f702300cf25d870d42b
Delvaux Paulin Niyonzima <s144849@student.dtu.dk>
2015-01-08 15:32:24 +0100

    Fix af fejl i Fængsel

---
+++ src/gameEngine/Fængsel.java
@@ -0,0 +1,25 @@
+package gameEngine;
+
+import boundaryToMatador.GUI;
+
+public class Fængsel extends Felt{
+
+	
+	private int PrisonPos = 11;
+	
+	public Fængsel(String fieldName){
+		super(fieldName);
+	}
+	/**
+	 * The play who landed on the field recieves a set bonus.
+	 */
+	@Override
+	public void landOnField(Spiller playerWhoLandedOnField) {
+		GUI.removeAllCars(playerWhoLandedOnField.getName());//Removes the player from the board.
+		//sets car at field corresponding to the value of the players position
+		GUI.setCar(PrisonPos, playerWhoLandedOnField.getName());
+		playerWhoLandedOnField.updateCurrentPos(-20);;
+		playerWhoLandedOnField.incrementPrisonCount();
+	}
+	
+}
