1ad7caa85a30f2df2c21a404255236245fa96a22
Kenneth Frandsen <Frandsen86@gmail.com>
2015-01-09 17:09:03 +0100

    Sat de andre Case op så man rykker frem eller tilbage. samt tækker penge fra alle. dog er der problemer med case 8 som jeg ikke kan få til at trække pengede igen så man ikke få de 4000 ved at passer start.

--- src/gameEngine/Metro.java
+++ src/gameEngine/Metro.java
@@ -10,7 +10,9 @@ public class Metro extends Felt{
 		private int metroBonus = 500;
 		private int rejsekortFine = 150;
 		private int plads[]={2,7,17,22,33,36};
-		private int update[]={6,11,6,11,4,7};
+		private int updatePositiv[]={6,11,6,11,4,7};
+		private int updateNegativ[]={35,-4,-9,-4,-10,-2};
+		private int updateDouble[]={16,16,17,15,10,12};
 		private int i = 0;
 		
 		public Metro(String fieldName){
@@ -22,7 +24,7 @@ public class Metro extends Felt{
 		
 		@Override
 		public void landOnField(Spiller playerWhoLanded) {
-			int result = (int)(1);//(int)(Math.random()*1)+ // 8 random Places for the Switch statsment. And +1 for make go from 1-8 and not 0-7.
+			int result =(int)(8);//  (int)((Math.random()*8)+1); // 8 random Places for the Switch statsment. And +1 for make go from 1-8 and not 0-7.
 			
 			while(playerWhoLanded.getCurrentPos() !=plads[i]){
 				i++;
@@ -34,298 +36,80 @@ public class Metro extends Felt{
 					System.out.println("Du har fået en bøde, for at køre uden billet. Betal " + metroFine +",-");
 					GUI.showMessage("Du har fået en bøde, for at køre uden billet. Betal " + metroFine +",-");
 					GUI.removeAllCars(playerWhoLanded.getName());
-					GUI.removeAllCars(playerWhoLanded.getName());
-					playerWhoLanded.updateCurrentPos(update [i]);
+					playerWhoLanded.updateCurrentPos(updatePositiv [i]);
 					GUI.setCar(playerWhoLanded.getCurrentPos(), playerWhoLanded.getName());
-					
-					
-//					
-//					if (playerWhoLanded.getCurrentPos() == i){
-//						GUI.removeAllCars(playerWhoLanded.getName());
-//						playerWhoLanded.updateCurrentPos(6);
-//						GUI.setCar(playerWhoLanded.getCurrentPos(), playerWhoLanded.getName());
-//					}
-//					else if(playerWhoLanded.getCurrentPos() == 7){
-//						GUI.removeAllCars(playerWhoLanded.getName());
-//						playerWhoLanded.updateCurrentPos(11);
-//						GUI.setCar(playerWhoLanded.getCurrentPos(), playerWhoLanded.getName());
-//					}
-//					else if(playerWhoLanded.getCurrentPos() == 17){
-//						GUI.removeAllCars(playerWhoLanded.getName());
-//						playerWhoLanded.updateCurrentPos(6);
-//						GUI.setCar(playerWhoLanded.getCurrentPos(), playerWhoLanded.getName());
-//					}
-//					else if(playerWhoLanded.getCurrentPos() == 22){
-//						GUI.removeAllCars(playerWhoLanded.getName());
-//						playerWhoLanded.updateCurrentPos(11);
-//						GUI.setCar(playerWhoLanded.getCurrentPos(), playerWhoLanded.getName());
-//					}
-//					else if(playerWhoLanded.getCurrentPos() == 33){
-//						GUI.removeAllCars(playerWhoLanded.getName());
-//						playerWhoLanded.updateCurrentPos(4);
-//						GUI.setCar(playerWhoLanded.getCurrentPos(), playerWhoLanded.getName());
-//					}
-//					else if(playerWhoLanded.getCurrentPos() == 36){
-//						GUI.removeAllCars(playerWhoLanded.getName());
-//						playerWhoLanded.updateCurrentPos(7);
-//						GUI.setCar(playerWhoLanded.getCurrentPos(), playerWhoLanded.getName());
-//					}
 					break;
+					
 				case 2:
+					System.out.println("Du har huske Rejsekortet idag. Ryk frem til Næste Station");
+					GUI.showMessage("Du har huske Rejsekortet idag. Ryk frem til Næste Station");
+					GUI.removeAllCars(playerWhoLanded.getName());
+					playerWhoLanded.updateCurrentPos(updatePositiv [i]);
+					GUI.setCar(playerWhoLanded.getCurrentPos(), playerWhoLanded.getName());
+					break;
+					
+				case 3:
+
 					playerWhoLanded.getAccount().deposit(metroBonus); //Deposit Bonus to the  players account
 					System.out.println("Du retuner en tegnebog til retmæssige ejer. Du får en findeløn " + metroBonus +",-");
 					GUI.showMessage("Du retuner en tegnebog til retmæssige ejer. Du får en findeløn " + metroBonus +",-");
-					if (playerWhoLanded.getCurrentPos() == 2){
-						GUI.removeAllCars(playerWhoLanded.getName());
-						playerWhoLanded.updateCurrentPos(6);
-						GUI.setCar(playerWhoLanded.getCurrentPos(), playerWhoLanded.getName());
-					}
-					else if(playerWhoLanded.getCurrentPos() == 7){
-						GUI.removeAllCars(playerWhoLanded.getName());
-						playerWhoLanded.updateCurrentPos(11);
-						GUI.setCar(playerWhoLanded.getCurrentPos(), playerWhoLanded.getName());
-					}
-					else if(playerWhoLanded.getCurrentPos() == 17){
-						GUI.removeAllCars(playerWhoLanded.getName());
-						playerWhoLanded.updateCurrentPos(6);
-						GUI.setCar(playerWhoLanded.getCurrentPos(), playerWhoLanded.getName());
-					}
-					else if(playerWhoLanded.getCurrentPos() == 22){
-						GUI.removeAllCars(playerWhoLanded.getName());
-						playerWhoLanded.updateCurrentPos(11);
-						GUI.setCar(playerWhoLanded.getCurrentPos(), playerWhoLanded.getName());
-					}
-					else if(playerWhoLanded.getCurrentPos() == 33){
-						GUI.removeAllCars(playerWhoLanded.getName());
-						playerWhoLanded.updateCurrentPos(4);
-						GUI.setCar(playerWhoLanded.getCurrentPos(), playerWhoLanded.getName());
-					}
-					else if(playerWhoLanded.getCurrentPos() == 36){
-						GUI.removeAllCars(playerWhoLanded.getName());
-						playerWhoLanded.updateCurrentPos(7);
-						GUI.setCar(playerWhoLanded.getCurrentPos(), playerWhoLanded.getName());
-					}
+					GUI.removeAllCars(playerWhoLanded.getName());
+					playerWhoLanded.updateCurrentPos(updatePositiv [i]);
+					GUI.setCar(playerWhoLanded.getCurrentPos(), playerWhoLanded.getName());
 					break;
-				case 3:
-
-					System.out.println("Du har taget Metroen den forkerte vej. Rykkes tilbage til Forige Station");
-					GUI.showMessage("Du har taget Metroen den forkerte vej. Rykkes tilbage til Forige Station");
-					if (playerWhoLanded.getCurrentPos() == 2){
-						GUI.removeAllCars(playerWhoLanded.getName());
-						playerWhoLanded.updateCurrentPos(35);
-						GUI.setCar(playerWhoLanded.getCurrentPos(), playerWhoLanded.getName());
-					}
-					else if(playerWhoLanded.getCurrentPos() == 7){
-						GUI.removeAllCars(playerWhoLanded.getName());
-						playerWhoLanded.updateCurrentPos(-4);
-						GUI.setCar(playerWhoLanded.getCurrentPos(), playerWhoLanded.getName());
-					}
-					else if(playerWhoLanded.getCurrentPos() == 17){
-						GUI.removeAllCars(playerWhoLanded.getName());
-						playerWhoLanded.updateCurrentPos(-9);
-						GUI.setCar(playerWhoLanded.getCurrentPos(), playerWhoLanded.getName());
-					}
-					else if(playerWhoLanded.getCurrentPos() == 22){
-						GUI.removeAllCars(playerWhoLanded.getName());
-						playerWhoLanded.updateCurrentPos(-4);
-						GUI.setCar(playerWhoLanded.getCurrentPos(), playerWhoLanded.getName());
-					}
-					else if(playerWhoLanded.getCurrentPos() == 33){
-						GUI.removeAllCars(playerWhoLanded.getName());
-						playerWhoLanded.updateCurrentPos(-10);
-						GUI.setCar(playerWhoLanded.getCurrentPos(), playerWhoLanded.getName());
-					}
-					else if(playerWhoLanded.getCurrentPos() == 36){
-						GUI.removeAllCars(playerWhoLanded.getName());
-						playerWhoLanded.updateCurrentPos(-2);
-						GUI.setCar(playerWhoLanded.getCurrentPos(), playerWhoLanded.getName());
-					}
-					break;
+					
 				case 4:
-
 					System.out.println("Du har huske Rejsekortet idag. Ryk frem til Næste Station");
 					GUI.showMessage("Du har huske Rejsekortet idag. Ryk frem til Næste Station");
-					if (playerWhoLanded.getCurrentPos() == 2){
-						GUI.removeAllCars(playerWhoLanded.getName());
-						playerWhoLanded.updateCurrentPos(6);
-						GUI.setCar(playerWhoLanded.getCurrentPos(), playerWhoLanded.getName());
-					}
-					else if(playerWhoLanded.getCurrentPos() == 7){
-						GUI.removeAllCars(playerWhoLanded.getName());
-						playerWhoLanded.updateCurrentPos(11);
-						GUI.setCar(playerWhoLanded.getCurrentPos(), playerWhoLanded.getName());
-					}
-					else if(playerWhoLanded.getCurrentPos() == 17){
-						GUI.removeAllCars(playerWhoLanded.getName());
-						playerWhoLanded.updateCurrentPos(6);
-						GUI.setCar(playerWhoLanded.getCurrentPos(), playerWhoLanded.getName());
-					}
-					else if(playerWhoLanded.getCurrentPos() == 22){
-						GUI.removeAllCars(playerWhoLanded.getName());
-						playerWhoLanded.updateCurrentPos(11);
-						GUI.setCar(playerWhoLanded.getCurrentPos(), playerWhoLanded.getName());
-					}
-					else if(playerWhoLanded.getCurrentPos() == 33){
-						GUI.removeAllCars(playerWhoLanded.getName());
-						playerWhoLanded.updateCurrentPos(4);
-						GUI.setCar(playerWhoLanded.getCurrentPos(), playerWhoLanded.getName());
-					}
-					else if(playerWhoLanded.getCurrentPos() == 36){
-						GUI.removeAllCars(playerWhoLanded.getName());
-						playerWhoLanded.updateCurrentPos(7);
-						GUI.setCar(playerWhoLanded.getCurrentPos(), playerWhoLanded.getName());
-					}
+					GUI.removeAllCars(playerWhoLanded.getName());
+					playerWhoLanded.updateCurrentPos(updatePositiv [i]);
+					GUI.setCar(playerWhoLanded.getCurrentPos(), playerWhoLanded.getName());
 					break;
+					
 				case 5:
 					playerWhoLanded.getAccount().withdraw(rejsekortFine); //withdraw to the players account
-
 					System.out.println("Du har ikke tjekket ud med Rejsekortet. Du få en bøde på." + rejsekortFine + ",-");
 					GUI.showMessage("Du har ikke tjekket ud med Rejsekortet. Du få en bøde på." + rejsekortFine + ",-");
-					if (playerWhoLanded.getCurrentPos() == 2){
-						GUI.removeAllCars(playerWhoLanded.getName());
-						playerWhoLanded.updateCurrentPos(6);
-						GUI.setCar(playerWhoLanded.getCurrentPos(), playerWhoLanded.getName());
-					}
-					else if(playerWhoLanded.getCurrentPos() == 7){
-						GUI.removeAllCars(playerWhoLanded.getName());
-						playerWhoLanded.updateCurrentPos(11);
-						GUI.setCar(playerWhoLanded.getCurrentPos(), playerWhoLanded.getName());
-					}
-					else if(playerWhoLanded.getCurrentPos() == 17){
-						GUI.removeAllCars(playerWhoLanded.getName());
-						playerWhoLanded.updateCurrentPos(6);
-						GUI.setCar(playerWhoLanded.getCurrentPos(), playerWhoLanded.getName());
-					}
-					else if(playerWhoLanded.getCurrentPos() == 22){
-						GUI.removeAllCars(playerWhoLanded.getName());
-						playerWhoLanded.updateCurrentPos(11);
-						GUI.setCar(playerWhoLanded.getCurrentPos(), playerWhoLanded.getName());
-					}
-					else if(playerWhoLanded.getCurrentPos() == 33){
-						GUI.removeAllCars(playerWhoLanded.getName());
-						playerWhoLanded.updateCurrentPos(4);
-						GUI.setCar(playerWhoLanded.getCurrentPos(), playerWhoLanded.getName());
-					}
-					else if(playerWhoLanded.getCurrentPos() == 36){
-						GUI.removeAllCars(playerWhoLanded.getName());
-						playerWhoLanded.updateCurrentPos(7);
-						GUI.setCar(playerWhoLanded.getCurrentPos(), playerWhoLanded.getName());
-					}
-
+					GUI.removeAllCars(playerWhoLanded.getName());
+					playerWhoLanded.updateCurrentPos(updatePositiv [i]);
+					GUI.setCar(playerWhoLanded.getCurrentPos(), playerWhoLanded.getName());
 					break;
+					
 				case 6:
 
 					System.out.println("Du tog Metroen den forkerte vej. Rykkes tilbage til Forige Station");
 					GUI.showMessage("Du tog Metroen den forkerte vej. Rykkes tilbage til Forige Station");
-					if (playerWhoLanded.getCurrentPos() == 2){
-						GUI.removeAllCars(playerWhoLanded.getName());
-						playerWhoLanded.updateCurrentPos(6);
-						GUI.setCar(playerWhoLanded.getCurrentPos(), playerWhoLanded.getName());
-					}
-					else if(playerWhoLanded.getCurrentPos() == 7){
-						GUI.removeAllCars(playerWhoLanded.getName());
-						playerWhoLanded.updateCurrentPos(11);
-						GUI.setCar(playerWhoLanded.getCurrentPos(), playerWhoLanded.getName());
-					}
-					else if(playerWhoLanded.getCurrentPos() == 17){
-						GUI.removeAllCars(playerWhoLanded.getName());
-						playerWhoLanded.updateCurrentPos(6);
-						GUI.setCar(playerWhoLanded.getCurrentPos(), playerWhoLanded.getName());
-					}
-					else if(playerWhoLanded.getCurrentPos() == 22){
-						GUI.removeAllCars(playerWhoLanded.getName());
-						playerWhoLanded.updateCurrentPos(11);
-						GUI.setCar(playerWhoLanded.getCurrentPos(), playerWhoLanded.getName());
-					}
-					else if(playerWhoLanded.getCurrentPos() == 33){
-						GUI.removeAllCars(playerWhoLanded.getName());
-						playerWhoLanded.updateCurrentPos(4);
-						GUI.setCar(playerWhoLanded.getCurrentPos(), playerWhoLanded.getName());
-					}
-					else if(playerWhoLanded.getCurrentPos() == 36){
-						GUI.removeAllCars(playerWhoLanded.getName());
-						playerWhoLanded.updateCurrentPos(7);
-						GUI.setCar(playerWhoLanded.getCurrentPos(), playerWhoLanded.getName());
-					}
+					GUI.removeAllCars(playerWhoLanded.getName());
+					playerWhoLanded.updateCurrentPos(updateNegativ [i]);
+					GUI.setCar(playerWhoLanded.getCurrentPos(), playerWhoLanded.getName());
 					break;
+					
 				case 7:
-
-					System.out.println("Du huske Ungdomskortet idag. Ryk frem til Næste Station");
-					GUI.showMessage("Du huske Ungdomskortet idag. Ryk frem til Næste Station");
-					if (playerWhoLanded.getCurrentPos() == 2){
-						GUI.removeAllCars(playerWhoLanded.getName());
-						playerWhoLanded.updateCurrentPos(6);
-						GUI.setCar(playerWhoLanded.getCurrentPos(), playerWhoLanded.getName());
-					}
-					else if(playerWhoLanded.getCurrentPos() == 7){
-						GUI.removeAllCars(playerWhoLanded.getName());
-						playerWhoLanded.updateCurrentPos(11);
-						GUI.setCar(playerWhoLanded.getCurrentPos(), playerWhoLanded.getName());
-					}
-					else if(playerWhoLanded.getCurrentPos() == 17){
-						GUI.removeAllCars(playerWhoLanded.getName());
-						playerWhoLanded.updateCurrentPos(6);
-						GUI.setCar(playerWhoLanded.getCurrentPos(), playerWhoLanded.getName());
-					}
-					else if(playerWhoLanded.getCurrentPos() == 22){
-						GUI.removeAllCars(playerWhoLanded.getName());
-						playerWhoLanded.updateCurrentPos(11);
-						GUI.setCar(playerWhoLanded.getCurrentPos(), playerWhoLanded.getName());
-					}
-					else if(playerWhoLanded.getCurrentPos() == 33){
-						GUI.removeAllCars(playerWhoLanded.getName());
-						playerWhoLanded.updateCurrentPos(4);
-						GUI.setCar(playerWhoLanded.getCurrentPos(), playerWhoLanded.getName());
-					}
-					else if(playerWhoLanded.getCurrentPos() == 36){
-						GUI.removeAllCars(playerWhoLanded.getName());
-						playerWhoLanded.updateCurrentPos(7);
-						GUI.setCar(playerWhoLanded.getCurrentPos(), playerWhoLanded.getName());
-					}
-
+					System.out.println("Du har huske Rejsekortet idag. Ryk frem til Næste Station");
+					GUI.removeAllCars(playerWhoLanded.getName());
+					playerWhoLanded.updateCurrentPos(updatePositiv [i]);
+					GUI.setCar(playerWhoLanded.getCurrentPos(), playerWhoLanded.getName());
 					break;
+					
 				case 8:
 					System.out.println("Du faldt i søvn i Metron. Du rykkes 2 Stationer frem. Ingen Bonus ved passering ved start");
 					GUI.showMessage("Du faldt i søvn i Metron. Du rykkes 2 Stationer frem. Ingen Bonus ved passering ved start");
-					if (playerWhoLanded.getCurrentPos() == 2){
-						GUI.removeAllCars(playerWhoLanded.getName());
-						playerWhoLanded.updateCurrentPos(16);
-						GUI.setCar(playerWhoLanded.getCurrentPos(), playerWhoLanded.getName());
-					}
-					else if(playerWhoLanded.getCurrentPos() == 7){
-						GUI.removeAllCars(playerWhoLanded.getName());
-						playerWhoLanded.updateCurrentPos(16);
-						GUI.setCar(playerWhoLanded.getCurrentPos(), playerWhoLanded.getName());
-					}
-					else if(playerWhoLanded.getCurrentPos() == 17){
-						GUI.removeAllCars(playerWhoLanded.getName());
-						playerWhoLanded.updateCurrentPos(16);
-						GUI.setCar(playerWhoLanded.getCurrentPos(), playerWhoLanded.getName());
-					}
-					else if(playerWhoLanded.getCurrentPos() == 22){
-						GUI.removeAllCars(playerWhoLanded.getName());
-						playerWhoLanded.updateCurrentPos(15);
-						GUI.setCar(playerWhoLanded.getCurrentPos(), playerWhoLanded.getName());
-					}
-					else if(playerWhoLanded.getCurrentPos() == 33){
-						GUI.removeAllCars(playerWhoLanded.getName());
-						playerWhoLanded.updateCurrentPos(10);
-						GUI.setCar(playerWhoLanded.getCurrentPos(), playerWhoLanded.getName());
+					GUI.removeAllCars(playerWhoLanded.getName());
+					playerWhoLanded.updateCurrentPos(updateDouble [i]);
+					GUI.setCar(playerWhoLanded.getCurrentPos(), playerWhoLanded.getName());
+					
+					if(playerWhoLanded.getCurrentPos() == 33 ){
 						playerWhoLanded.getAccount().withdraw(-4000);
+						System.out.println("Trækker 4000 fra for at passer start");
 					}
-					else if(playerWhoLanded.getCurrentPos() == 36){
-						GUI.removeAllCars(playerWhoLanded.getName());
-						playerWhoLanded.updateCurrentPos(12);
-						GUI.setCar(playerWhoLanded.getCurrentPos(), playerWhoLanded.getName());
-						playerWhoLanded.getAccount().withdraw(-4000);
+					else if(playerWhoLanded.getCurrentPos() == 36 ){
+							playerWhoLanded.getAccount().withdraw(-4000);
+							System.out.println("Trækker 4000 fra for at passer start");
 					}
 					break;
 					
 				}
-				
-					
-				
-				
+
 				}
 				
 		
